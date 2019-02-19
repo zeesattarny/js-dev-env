873e6eceb6ed27646ce99cc76504de4fc4b40da0
Zeeshan Sattar <zeeshan.sattar@tntp.org>
2019-02-19 15:20:02 -0500

    more modifcations to the framework

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,6 +21,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req,res) {
+  // Hard coding for simplicity. Pretend this is a real database
+  res.json([
+    {"id": 1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id": 1, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@gmail.com"},
+    {"id": 1, "firstName":"Tina", "lastName":"Lee", "email":"tina@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if(err) {
     console.log(err);
--- src/index.test.js
+++ src/index.test.js
@@ -10,12 +10,11 @@ describe('Our first test', () => {
 
 
 describe('index/html', () => {
-  it('should say hello', (done) => {
+  it('should have h1 that says Users', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      console.log(h1);
-      expect(h1.describe).to.equal("Hello World!");
+      expect(h1.describe).to.equal("Users");
       done();
       window.close();
     });
